cac19ee255aa859f40393c68d7101172abe9c01b
Cayla Schwartz <cayschwartz@gmail.com>
2017-08-29 23:27:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  var newkit[] = kittens + [name]
+  var newkit = kittens[] + [name]
   console.log(newkit)
   return newkit
 }
